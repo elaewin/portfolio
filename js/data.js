c4c965f5038332a9216ac605e99cef21fca22c8d
Erica Winberry <elaewin@gmail.com>
2016-07-12 22:04:53 -0700

    social media links in posts are now linking correctly to the specific project/website.

--- js/data.js
+++ js/data.js
@@ -1,27 +1,27 @@
 'use strict';
 
-var data = [
+var postData = [
   {
-    title: 'Eat Fellows',
+    title: 'Group Project: Eat Fellows',
     category: 'classwork',
-    date: '2016-07-09',
+    pubDate: '2016-07-10',
     website: 'https://elaewin.github.io/eat_fellows/',
     githubRepo: 'https://github.com/elaewin/eat_fellows',
-    content: '<p>Week 4 of Code Fellows 201 was spent working on a group project. My team built a site that gives CF students a map of local places to go for lunch.</p><p>The project had the following requirements (from the description of the project):</p><ul><li>There can be no more than 3-4 people per team.</li><li>Each person must make meaningful contributions in HTML, CSS, and JavaScript.</li><li>Your web site must have at least 3 interlinked pages and clear navigation.</li><li>Your web site must have at least 2 pages that accept and process user input.</li><li>Your web site\'s state must persist between page reloads.</li><li>Your web site must be deployed live on the Internet.</li></p><p>The project used only local storage to save new restaurants and reviews, since working with databases is not within the scope of the 201 class. I worked on the project with <a href="https://github.com/Bl41r">David</a>, <a href="https://github.com/vcraig">Val</a>, and <a href="https://github.com/loganlsr">Logan</a></p>'
+    postContent: '<p>Week 4 of Code Fellows 201 was spent working on a group project. My team built a site that gives CF students a map of local places to go for lunch.</p><p>The project had the following requirements (from the description of the project):</p><ul><li>There can be no more than 3-4 people per team.</li><li>Each person must make meaningful contributions in HTML, CSS, and JavaScript.</li><li>Your web site must have at least 3 interlinked pages and clear navigation.</li><li>Your web site must have at least 2 pages that accept and process user input.</li><li>Your web site\'s state must persist between page reloads.</li><li>Your web site must be deployed live on the Internet.</li></p><p>The project used only local storage to save new restaurants and reviews, since working with databases is not within the scope of the 201 class. I worked on the project with <a href="https://github.com/Bl41r">David</a>, <a href="https://github.com/vcraig">Val</a>, and <a href="https://github.com/loganlsr">Logan</a></p>'
   },
   {
-    title: 'BusMall',
+    title: 'Lab Assignment: BusMall',
     category: 'classwork',
-    date: '2016-07-01',
+    pubDate: '2016-07-01',
     website: 'https://elaewin.github.io/bus_mall/',
     githubRepo: 'https://github.com/elaewin/bus_mall',
-    content: '<p>My week 3 Code Fellows lab project. The project simulates a marketing survey for a company that wants to build a SkyMall-style buisiness based on local transit. The goal of the project was to learn how to work with local storage, JSON files, and get some practice working with JavaScript libraries, in particular the <a href="http://www.chartjs.org/">Chart.js</a> library.</p><p>From the lab assignment:</p><p>To make this market analysis maximally effective, BusMall wants you to build an app that displays potential products to individuals in focus groups (three products at a time, side-by-side-by-side, so you\'ll need to manage the size and the aspect ratio of the images and perhaps edit them a bit; Mac users can do this in Preview (very cool!), plus, there are lots of online tools.</p><p>The app\'s purpose is to have the group members choose which product, of the three displayed images, that they would be most likely to purchase, and then store, calculate, and visually display the resulting data.</p><p>To keep the product selection process as untainted as possible, you have been instructed to not allow any results to be shown to users until there have been a total of 25 selections made.</p><p>The marketing team is not only interested in the total number of clicks, but also the percentage of times that an item was clicked when it was shown. So, you\'ll also need to keep track of how many times each image is displayed and do the calculations.</p><p>You are also responsible for the look and feel of the app, so don\'t forget a custom font, color palette, layout with semantic HTML, and so on.</p>'
+    postContent: '<p>My week 3 Code Fellows lab project. The project simulates a marketing survey for a company that wants to build a SkyMall-style buisiness based on local transit. The goal of the project was to learn how to work with local storage, JSON files, and get some practice working with JavaScript libraries, in particular the <a href="http://www.chartjs.org/">Chart.js</a> library.</p><p>From the lab assignment:</p><p>&#8220;To make this market analysis maximally effective, BusMall wants you to build an app that displays potential products to individuals in focus groups (three products at a time, side-by-side-by-side, so you\'ll need to manage the size and the aspect ratio of the images and perhaps edit them a bit; Mac users can do this in Preview (very cool!), plus, there are lots of online tools.</p><p>&#8220;The app\'s purpose is to have the group members choose which product, of the three displayed images, that they would be most likely to purchase, and then store, calculate, and visually display the resulting data.</p><p>&#8220;To keep the product selection process as untainted as possible, you have been instructed to not allow any results to be shown to users until there have been a total of 25 selections made.</p><p>The marketing team is not only interested in the total number of clicks, but also the percentage of times that an item was clicked when it was shown. So, you\'ll also need to keep track of how many times each image is displayed and do the calculations.</p><p>&#8220;You are also responsible for the look and feel of the app, so don\'t forget a custom font, color palette, layout with semantic HTML, and so on.&#8221;</p>'
   },
   {
     title: 'Learning Journal',
     category: 'blog',
-    date: '2016-07-10',
+    pubDate: '2016-07-09',
     githubRepo: 'https://github.com/elaewin/learning_journal',
-    content: 'One of my assignments for CF 201 was a daily learning journal, used to reflect on what I\'d learned on any given day. I tried to put in at least one small bit of code each day, as well as bullet point "things to remember".'
-  }
+    postContent: 'One of my assignments for CF 201 was a daily learning journal, used to reflect on what I\'d learned on any given day. I tried to put in at least one small bit of code each day, as well as bullet point &#8220;things to remember&#8221;.'
+  },
 ];
---
+++ js/post.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var postsArray = []
+
+function Post(source) {
+  this.title = source.title;
+  this.category = source.category;
+  this.pubDate = source.pubDate;
+  this.website = source.website || 'none';
+  this.githubRepo = source.githubRepo || 'none';
+  this.postContent = source.postContent;
+}
+
+Post.prototype.toHtml = function() {
+  var $newPost = $('article.template').clone();
+  $newPost.find('h2').text(this.title);
+  $newPost.attr('category', this.category);
+  if(this.website !== 'none') {
+    $newPost.find('.link a').attr('href', this.website);
+  } else {
+    $newPost.find('.link').addClass('grey');
+  };
+  if(this.githubRepo !== 'none') {
+    $newPost.find('.repo a').attr('href', this.githubRepo);
+  } else {
+    $newPost.find('.repo').addClass('grey');
+  };
+  $newPost.find('.content_body').html(this.postContent);
+
+  // Based on the example from the lab on 7/12.
+  $newPost.find('time[pubdate]').attr('title', this.pubDate);
+  $newPost.find('time').html('about ' + parseInt((new Date() - new Date(this.pubDate))/60/60/24/1000) + ' days ago');
+
+  $newPost.removeClass('template').addClass('post_content');
+
+  return ($newPost);
+};
+
+// Sorts by pubDate, ascending
+postData.sort(function(a,b) {
+  return (new Date(b.pubDate)) - (new Date(a.pubDate));
+});
+
+// Creates new Post objects and pushes them to postsArray.
+postData.forEach(function(obj) {
+  postsArray.push(new Post(obj));
+});
+
+postsArray.forEach(function(post) {
+  $('#posts').append(post.toHtml());
+});
