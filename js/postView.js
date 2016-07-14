d4c97c388511fc117729a8e248307a3eeb3ff388
Erica Winberry <elaewin@gmail.com>
2016-07-13 21:39:38 -0700

    cleaning out comments in js file, moving 'about' section to the bottom of index.html.

--- js/postView.js
+++ js/postView.js
@@ -45,21 +45,3 @@ postView.handleCategoryFilter = function() {
 postView.populateFilters();
 postView.handleCategoryFilter();
 postView.handleMainNav();
-/* 1. As the creator, I want the Home and About nav links to act as tabs, so my
-      story is revealed FAST.
-    * This means your links to NOT navigate to a new page.
-    * Instead, your "single page app" shows only the section related to the
-      navigation tab that is selected.
-    * You can use 'data-' attributes to associate a content section with a
-      particular tab
-    * Then use jQuery so when the tab is clicked, you hide all the sections,
-      then reveal the associated section only.
-
-STRETCH GOALS
-1. As a reader, I want any lengthy descriptions truncated to the first paragraph
-   so that I can easily scroll though the whole list.
-2. As a reader, I want to click the "More" button so that I can expand the
-   entire description.
-3. As a reader, I want projects filterable by category so that I can review just
-   the things that interest me.
-*/
