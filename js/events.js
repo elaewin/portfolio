fcaf03b58bf3de72080eaf9ac781bdec1d4e4117
Erica Winberry <elaewin@gmail.com>
2016-07-16 23:04:44 -0700

    Moved handleMainNav to events.js. Fixed issue where selecting a menu item in the list on smaller screens would not close the hamburger menu.

--- js/events.js
+++ js/events.js
@@ -1,10 +1,32 @@
 'use strict';
 
-function hamburgerMenu() {
+var pageEvents = {};
+
+pageEvents.hamburgerMenu = function() {
   $('.main-nav div').on('click', function() {
     $(this).toggleClass('icon-cross');
     $('.main-nav ul').toggle();
   });
+  //
+  // $('.main-nav').on('click', '.tab', function() {
+  //   if($);
+  // });
 };
 
-hamburgerMenu();
+pageEvents.handleMainNav = function() {
+  $('.main-nav').on('click', '.tab', function() {
+    var $selectedTab = $(this).attr('data-content');
+    var $hamburger = $('.main-nav div');
+    $('section.tab-content').hide();
+    $('section.tab-content[id="' + $selectedTab + '"]').fadeIn('slow');
+    if($hamburger.hasClass('icon-cross')) {
+      $hamburger.toggleClass('icon-cross');
+      console.log($(this).parent());
+      $(this).parent().fadeOut();
+    }
+  });
+  $('.main-nav .tab:last').click();
+};
+
+pageEvents.hamburgerMenu();
+pageEvents.handleMainNav();
