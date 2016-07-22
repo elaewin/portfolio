22979894ac8c5cb47f4e828e701183a9bb43c84a
Erica Winberry <elaewin@gmail.com>
2016-07-22 10:33:56 -0700

    refactored Post.getAllData to take a url.

--- js/post.js
+++ js/post.js
@@ -24,7 +24,7 @@
     });
   };
 
-  Post.fetchAll = function() {
+  Post.fetchAll = function(nextFunction) {
     if (localStorage.postData) {
       $.ajax({
         url: 'data/postData.json',
@@ -32,24 +32,24 @@
         success: function(data,response,xhr) {
           var eTag = xhr.getResponseHeader('ETag');
           if(!localStorage.eTag || eTag !== localStorage.eTag) {
-            Post.getAllData();
+            Post.getAllData('data/postData.json');
           } else {
             Post.loadAll(JSON.parse(localStorage.postData));
-            postView.renderIndexPage();
+            nextFunction();
           }
         }
       });
     } else {
-      Post.getAllData();
+      Post.getAllData('data/postData.json');
     }
   };
 
-  Post.getAllData = function() {
-    $.getJSON('data/postData.json', function(data, response, xhr) {
+  Post.getAllData = function(url) {
+    $.getJSON('url', function(data, response, xhr) {
       localStorage.postData = JSON.stringify(data);
       localStorage.eTag = xhr.getResponseHeader('ETag');
       Post.loadAll(data);
-      postView.renderIndexPage();
+      nextFunction();
     });
   };
 
