9d10c0b667529c841fa6596119ed5da1be67234f
Erica Winberry <elaewin@gmail.com>
2016-07-27 11:37:58 -0700

    Working on getting data to load.

--- js/view/githubView.js
+++ js/view/githubView.js
@@ -1,11 +1,15 @@
 'use strict';
 
 (function(module) {
-
   var githubView = {};
 
+  var blogCompiler = Handlebars.compile($('#blog-template').text());
 
-
+  githubView.renderBlogEntry = function() {
+    $('#blog').empty().append(
+      githubObj.ghData.map(blogCompiler)
+    );
+  };
 
   module.githubView = githubView;
 })(window);
