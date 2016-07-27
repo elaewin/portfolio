785064a62f302524a4d8c0b64d08bfbecc8f788e
Erica Winberry <elaewin@gmail.com>
2016-07-27 11:16:35 -0700

    working on getting files, still trying to figure out how to get all files from a repo one by one.

--- js/model/github.js
+++ js/model/github.js
@@ -6,7 +6,21 @@
 
   githubObj.ghData = [];
 
-  
+  githubObj.requestData = function(callback) {
+    $.ajax({
+      url: 'https://api.github.com/repos/elaewin/learning_journal_201',
+      type: 'GET',
+      headers: {
+        // 'Accept': 'application/vnd.github.v3.raw',
+        'Authorization': 'token ' + githubToken,
+      },
+      success: function(data, message, xhr) {
+        console.log(data);
+      }
+    });
+  };
+
+  githubObj.requestData();
 
   module.githubObj = githubObj;
 })(window);
