1bb4bc4b26554c0f0078abc734e82373d7d5d24f
Erica Winberry <elaewin@gmail.com>
2016-07-27 13:18:25 -0700

    working on getting the files from my 201 lj thanks to help from Liz, need to figure out the regex.

--- js/model/github.js
+++ js/model/github.js
@@ -8,16 +8,31 @@
 
   githubObj.requestData = function(callback) {
     $.ajax({
-      url: 'https://api.github.com/repos/elaewin/learning_journal_201',
+      url: 'https://api.github.com/repos/elaewin/learning_journal_201/contents',
       type: 'GET',
       headers: {
-        // 'Accept': 'application/vnd.github.v3.raw',
         'Authorization': 'token ' + githubToken,
       },
-      success: function(data, message, xhr) {
-        console.log(data);
+      success: function(fileData, message, xhr) {
+        fileData.forEach(function(current) {
+          var re = 'lj' + (/\w+/);
+          if(current.name === re) {
+            $.ajax({
+              url: current.url,
+              type: 'GET',
+              headers: {
+                'Accept': 'application/vnd.github.v3.raw',
+                'Authorization': 'token ' + githubToken,
+              },
+              success: function(urlData, message, xhr) {
+                console.log(urlData);
+              }
+            });
+          }
+        });
       }
     });
+    console.log(githubObj.ghData);
   };
 
   githubObj.requestData();
